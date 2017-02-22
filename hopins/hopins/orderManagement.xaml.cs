c515d6619d41213096e619b4025eef6b99400f56
yosefbrian <yosefbrian@gmail.com>
2017-02-22 22:24:10 +0700

    tampilan awal

--- hopins/hopins/orderManagement.xaml.cs
+++ hopins/hopins/orderManagement.xaml.cs
@@ -13,33 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace hopins
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class orderManagement : Page
     {
-        public MainPage()
+        public orderManagement()
         {
             this.InitializeComponent();
-        }
-
-        private void btAdd_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(myProduct), null);
-        }
-
-        private void btProduct_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(addProduct), null);
-        }
-
-        private void btOrder_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(orderManagement), null);
         }
     }
 }
