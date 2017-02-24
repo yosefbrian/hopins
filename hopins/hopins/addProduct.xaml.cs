353191bbd581c2e44be19df1376c03efec2bd15e
yosefbrian <yosefbrian@gmail.com>
2017-02-24 22:01:19 +0700

    camera embeded

--- hopins/hopins/addProduct.xaml.cs
+++ hopins/hopins/addProduct.xaml.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,14 +29,52 @@ namespace hopins
     /// </summary>
     public sealed partial class addProduct : Page
     {
+
         public addProduct()
         {
             this.InitializeComponent();
         }
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+
+        Windows.Media.Capture.MediaCapture captureManager;
+
+        async private void InitCamera_Click(object sender, RoutedEventArgs e)
         {
-            await Windows.System.Launcher.LaunchUriAsync(new Uri("microsoft.windows.camera:"));
+            captureManager = new MediaCapture();
+            await captureManager.InitializeAsync();
         }
+
+        async private void StartCapturePreview_Click(object sender, RoutedEventArgs e)
+        {
+            capturePreview.Source = captureManager;
+            await captureManager.StartPreviewAsync();
+        }
+
+        async private void StopCapturePreview_Click(object sender, RoutedEventArgs e)
+        {
+            await captureManager.StopPreviewAsync();
+        }
+
+        async private void CapturePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            ImageEncodingProperties imgFormat = ImageEncodingProperties.CreateJpeg();
+
+            // create storage file in local app storage
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                "TestPhoto.jpg",
+                CreationCollisionOption.GenerateUniqueName);
+
+            // take photo
+            await captureManager.CapturePhotoToStorageFileAsync(imgFormat, file);
+
+            // Get photo as a BitmapImage
+            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+
+            // imagePreivew is a <Image> object defined in XAML
+            imagePreivew.Source = bmpImage;
+        }
+
+
+
     }
 }
--- hopins/hopins/myProduct.xaml.cs
+++ hopins/hopins/myProduct.xaml.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,5 +30,47 @@ namespace hopins
         {
             this.InitializeComponent();
         }
+
+        Windows.Media.Capture.MediaCapture captureManager;
+
+        async private void InitCamera_Click(object sender, RoutedEventArgs e)
+        {
+            captureManager = new MediaCapture();
+            await captureManager.InitializeAsync();
+        }
+
+        async private void StartCapturePreview_Click(object sender, RoutedEventArgs e)
+        {
+            capturePreview.Source = captureManager;
+            await captureManager.StartPreviewAsync();
+        }
+
+        async private void StopCapturePreview_Click(object sender, RoutedEventArgs e)
+        {
+            await captureManager.StopPreviewAsync();
+        }
+
+        async private void CapturePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            ImageEncodingProperties imgFormat = ImageEncodingProperties.CreateJpeg();
+
+            // create storage file in local app storage
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                "TestPhoto.jpg",
+                CreationCollisionOption.GenerateUniqueName);
+
+            // take photo
+            await captureManager.CapturePhotoToStorageFileAsync(imgFormat, file);
+
+            // Get photo as a BitmapImage
+            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+
+            // imagePreivew is a <Image> object defined in XAML
+            imagePreivew.Source = bmpImage;
+        }
+
+
+
+
     }
 }
